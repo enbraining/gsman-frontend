61f133ccdaecce2238c8d5744638a1be70274b29
enbraining <me@fodo.dev>
2024-07-31 21:55:55 +0900

    :recycle: 실수로 제거하지 않은 채널톡 스크립트 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import LocalFont from "next/font/local";
-import Script from "next/script";
 import Header from "./components/Header";
 import "./globals.css";
 
@@ -20,18 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script
-        id="channelTalk"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
-
-          ChannelIO('boot', {
-            "pluginKey": "${process.env.CHANNEL_IO_TOKEN}"
-          });`,
-        }}
-      />
       <body className={inter.className}>
         <Header />
         {children}
